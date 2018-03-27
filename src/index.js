d30269f390ef8912e65e5a1320b7bf4e0f1d499e
Cristian Andrei <zink.krysty@gmail.com>
2018-03-27 21:21:18 +0200

    Webpack guides: Prepare for output management

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.svg';
 
 function component() {
   var element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  let myIcon = new Image();
-  myIcon.src = Icon;
-  myIcon.width = 100;
-
-  element.appendChild(myIcon);
 
   return element;
 }
